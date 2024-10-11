c86c05827209a8496f14b983fefe832074af50a2
Pawel Elert <pawelelert@wp.pl>
2024-10-11 23:45:04 +0200

    update of variables_comments_data_types

--- various_tests/variables_comments_data_types.py
+++ various_tests/variables_comments_data_types.py
@@ -1,3 +1,32 @@
 num_1 = 50
 
 print(num_1)
+
+num_2 = 124
+
+num_notexists = 42
+print(num_notexists)
+
+some_var = 1001
+
+print(some_var)
+
+some_var = "Mango"
+print(some_var)
+
+x = 5           # x is an integer
+y = "Hello"     # y is a string
+z = 3.14        # z is a float
+
+x, y, z
+
+my_variable = "hello world"
+
+print(isinstance(my_variable, str))
+
+my_variable = 10
+print(type(my_variable))
+
+my_string = "Hello, world!"
+
+print(type(my_string))
