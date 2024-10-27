1fb1674acf8f80b06e6ac52be308d03d4010f7af
Pawel Elert <pawelelert@wp.pl>
2024-10-27 21:41:54 +0100

    working on lambdas

--- various_tests/lambdas.py
+++ various_tests/lambdas.py
@@ -1,3 +1,22 @@
-def increment(x):
-    return x + 1
+# def increment(x):
+#     return x + 1
 
+# print(increment)
+
+# print(increment(4))
+
+# print(increment(411.3838))
+
+plus_one = lambda x: x + 1
+
+print(plus_one)
+
+print(plus_one(6))
+
+print(plus_one(101.3))
+
+multiply_by_10 = lambda a: a * 10
+
+print(multiply_by_10)
+
+print(multiply_by_10(23))
